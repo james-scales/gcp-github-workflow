23207ee371d6c08d939eabd46bcab6af4ad94cb5
James Scales <jescalesjr1987@gmail.com>
2025-11-28 23:02:44 -0500

    code update: v3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GCP CI/CD demo v2.2!"
+    return "Hello from GCP CI/CD demo v3! I got this to work!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
